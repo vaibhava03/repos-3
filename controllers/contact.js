3522b33b44275ca56add309cf25f85968d59dc91
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-12 18:06:33 +0530

    Update contact.js

--- controllers/contact.js
+++ controllers/contact.js
@@ -2,7 +2,7 @@
 //controllers
 
 const path=require('path');
-const Product=require('../models/product');
+
 exports.getContacts=(req, res, next) =>{
        
     res.sendFile(path.join(__dirname,'../', 'views', 'contact', 'contactUS.html'));
