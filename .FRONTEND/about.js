ded4c8020e2e028a1f4a7ab95664dd989d845149
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-12 17:57:40 +0530

    Create about.js

---
+++ .FRONTEND/about.js
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce Website</title>
+    <link rel="stylesheet" href="e-comm.css">
+    <script src="e-comm.js" defer></script>
+
+</head>
+<body>
+    <header class="header">
+<nav class="navig">
+<ul class="ulClass">
+    <li class="li"><a href="home.html">HOME</a></li>
+    <li class="li"><a href="store.html">STORE</a></li>
+    <li class="li"><a class="active" href="about.html">ABOUT</a></li>
+</ul>
+</nav>
+
+<div class="div1">
+    <h1> The Generics</h1>
+</div>
+</header>
+<main>
+<section class="about">
+
+    <h3>ABOUT</h3>
+    <p>
+        Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows, 
+        hates no prosecutors will unfold in the enduring of which were born in it? 
+        Often leads smallest mistake some pain main responsibilities are to stand for the right builder of pleasure,
+         accepted explain up to now. , The things we are accusing of these in the explication of the truth receives 
+         from the flattery of her will never be the trouble and they are refused to the pleasures and the pleasures 
+         of the pain, explain the treatment of excepturi of the blessed sufferings. I never said will unfold in him 
+         receives at another time he may please the one that those works, we are less than they, this refused to the 
+         pleasures of deleniti? Those are! Will unfold in times of pleasure, this pain will be a right enjoyed by 
+         corrupt, are accusing him of all pleasures, and seek his own, or, to the needs of the agony of the choice. 
+         We hate the fellow.
+Lorem ipsum dolor, sit amet consectetur rebates. The distinction, that arise from or to. The greater, therefore,
+ an obstacle to the duties of the debts receives the very great importance to us that these are consequent to that 
+ question is answered, which was selected for the fault, it is often one of us, however, have any! Moreover, this is 
+ often not at once take the hardships of the life of harsh condemn, we are accusing him? Him whom something large cisterns.
+    </p>
+</section>
+</main>
+<footer class="footer">
+    <h1>The Generics</h1>
+    <div class="div13">
+        <ul class="ul-class2">
+            <li class="li-class">
+                <a href="https://www.youtube.com"><img src="youtubelogo.jpg" class="img"></a>
+                <li class="li-class">
+                    <a href="https://www.spotify.com"><img src="Spotify Logo.png" class="img"></a>
+                </li>
+                <li class="li-class">
+                    <a href="https://www.facebook.com"><img src="Facebook Logo.png" class="img"></a>
+                </li>
+            </li>
+        </ul>
+    </div>
+
+</footer>
+</body>
+</html>
