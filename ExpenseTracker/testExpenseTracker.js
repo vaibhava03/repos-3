a2d5d9306b9e7b5a1853ba3ed0570a5bc845a025
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-17 21:28:55 +0530

    Update testExpenseTracker.js

--- ExpenseTracker/testExpenseTracker.js
+++ ExpenseTracker/testExpenseTracker.js
@@ -7,18 +7,19 @@
    const sequelize=require('./util/database');
    const Expense=require('./models/expense');
    const User=require('./models/user');
+   const FPReq=require('./models/ForgotPasswordRequests');
    const cors=require('cors');
    const bcrypt = require('bcrypt');
    const jwt = require('jsonwebtoken');
    const Order=require('./models/order');
    const Razorpay=require('razorpay');
-const { default: axios } = require('axios');
    const instance = new Razorpay({ key_id: 'rzp_test_PEFKbA3GQ0O6x9', key_secret: 'iadpASiQ052e0z1SuvrFSTlR' });
    const sgMail = require('@sendgrid/mail');
-const { getMaxListeners } = require('process');
-const { create } = require('domain');
-   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+   const { v4: uuidv4 } = require('uuid');
 
+
+
+   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
    app.use(cors());
    app.use(bodyParser.json());
 
@@ -26,6 +27,8 @@ const { create } = require('domain');
    Expense.belongsTo(User);
    User.hasMany(Order);
    Order.belongsTo(User);
+   User.hasMany(FPReq);
+   FPReq.belongsTo(User);
 
    function parseJwt(token) {
       var base64Payload = token.split('.')[1];
@@ -150,7 +153,6 @@ res.sendStatus(200);
 
    });
 
-
    const data=[];
   User.findAll()
   .then(users =>{
@@ -169,6 +171,7 @@ res.sendStatus(200);
      });
   })
 
+
 app.get('/user/leaderboard', (req, res) =>{
    setTimeout(() =>{
    res.json(data.sort(function(x,y){return y.expenses-x.expenses}));
@@ -178,18 +181,81 @@ app.get('/user/leaderboard', (req, res) =>{
 
 
 app.post('/user/password/forgotpassword',(req, res) =>{
+User.findOne({where:{email:req.body.email}})
+.then(user =>{
+   const FP_id=uuidv4();
+   user.createFPReq({id:FP_id,isActive:true});
 const msg={
    to:req.body.email,
-   from:"sender@email.com",
+   from:"vaibhavavarikilla@gmail.com",
    subject:"create new password",
-   text: "hi create a new password here",
-   html:'<strong>reset password link</strong>',
+   text: "hi we received a reset password request",
+   html:`<strong>click the link to reset password</strong><a href=http://localhost:3000/password/resetpassword/${FP_id}>click here</a>`
 }
 sgMail.send(msg)
 .then((response) =>{
-   console.log(response);
+   // console.log(response);
 })
 })
+})
+
+app.use('/password/resetpassword/:passwordId', (req, res) =>{
+   FPReq.findOne({where:{id:req.params.passwordId}})
+   .then(data =>{
+      if(data&&data.isActive){
+
+     res.status(200).send(`<html>
+   
+     <form action="/updatepassword/${req.params.passwordId}" method="GET">
+     <label for="password">Enter a new password</label>
+     <input type="password" name="password">
+     <button type="submit">SUBMIT</button>
+     </form>
+  
+     </html>`);
+     console.log(req.body);
+     data.update({ isActive: false});
+     res.end()
+
+      }
+      else console.log(res.status);
+   })
+   .catch(err => console.log(err));
+  
+   // res.redirect('/updatepassword/${req.body}')
+});
+
+app.use('/updatepassword/:resetpasswordid', (req, res) =>{
+   console.log(req.query.password);
+   console.log(req.params.resetpasswordid);
+const password=req.query.password;
+const Id=req.params.resetpasswordid;
+FPReq.findOne({where:{id:Id}})
+.then(Frequest =>{
+   if(Frequest)
+   {
+      console.log(Frequest.userId)
+      User.findOne({where:{id:Frequest.userId}})
+      then(user =>{
+         console.log(user);
+         bcrypt.genSalt()
+         .then(salt =>{
+               bcrypt.hash(password, salt).then(hash =>{
+               user.update({
+               password:hash
+      })
+})
+
+})
+      })
+   }
+   return res.status(404).json('No user Exists');
+})
+.catch(err => {
+   return res.status(403);
+})
+
+});
 
    sequelize
     .sync()
